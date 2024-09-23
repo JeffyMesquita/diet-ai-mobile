3cf68e01fa7c9cdc756f97f11761705b0cb10dd6
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-09-22 22:25:03 -0300

    feat: home done

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,72 @@
-import { Text, View } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { colors } from "@/constants/colors";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/images/logo.png")} />
+
+      <Text style={styles.title}>
+        Dieta
+        <Text
+          style={{
+            color: colors.white,
+          }}
+        >
+          .IA
+        </Text>
+      </Text>
+
+      <Text style={styles.text}>
+        Sua dieta personalizada com base em inteligência artificial.
+      </Text>
+
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          console.log("Gerar dieta");
+        }}
+      >
+        <Text style={styles.buttonText}>Gerar Dieta</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: colors.green,
+  },
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    textAlign: "center",
+    marginTop: 16,
+    width: "80%",
+    marginVertical: 8,
+  },
+  button: {
+    backgroundColor: colors.blue,
+    width: "100%",
+    height: 40,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 34,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
---
+++ app/step/index.tsx
@@ -0,0 +1,4 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { colors } from "@/constants/colors";
+
+export default function Step() {}
